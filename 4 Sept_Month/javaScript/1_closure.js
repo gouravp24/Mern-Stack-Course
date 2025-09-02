38503c4ef35a8d37e188162c91c4c5f5e9e7b488
gouravp24 <gouravupatil@gmail.com>
2025-09-03 02:52:53 +0530

    closure commit

---
+++ 4 Sept_Month/javaScript/1_closure.js
@@ -0,0 +1,20 @@
+function firstFunction() {
+  const name = "Gourav";
+
+  function secondFunction() {
+    const surname = "Patil";
+
+    var number = 12;
+
+    if (true) {
+      var finalNumber = 50;
+    }
+
+    console.log(finalNumber);
+
+    console.log(name + " " + surname);
+  }
+  console.log(name, "sanjay");
+  secondFunction();
+}
+firstFunction();
